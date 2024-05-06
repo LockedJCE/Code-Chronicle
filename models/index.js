dee431c92f06ad663143d080cbb0ea46968df590
Jensen <163614828+LockedJCE@users.noreply.github.com>
2024-05-05 18:13:38 -0700

    added logout and profile js as well as connected my bpost, homepage, profile, and main handlebars

--- models/index.js
+++ models/index.js
@@ -1,1 +1,13 @@
 const User = require('/User');
+const Bpost = require('/Bpost');
+
+User.hasMany(Bpost, {
+  foreignKey: 'user_id',
+  onDelete: 'CASCADE'
+});
+
+Project.belongsTo(User, {
+  foreignKey: 'user_id'
+});
+
+module.exports = { User, Bpost };
